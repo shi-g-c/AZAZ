1f959013fae3d3e3470328225294d061f81a8a8b
WindJerry <99171168@qq.com>
2023-10-25 17:20:50 +0800

    service基础构建完成

---
+++ azaz-service/azaz-interact/src/main/java/com/azaz/InteractApplication.java
@@ -0,0 +1,15 @@
+package com.azaz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.text.html.HTMLDocument;
+
+@SpringBootApplication
+@Slf4j
+public class InteractApplication {
+    public static void main(String[]args){
+        SpringApplication.run(InteractApplication.class);
+    }
+}
